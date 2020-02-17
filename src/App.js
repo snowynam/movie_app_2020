e5a6d9fef26e1c97cedba34f7a1ba6475b141c8d
snowynam <snowynam1@gmail.com>
2020-02-17 23:50:50 +0900

    Add potato.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Potato from './Potato';
 
 function App() {
-  return <div className="App"><h1>Hello world!!</h1></div>
+  return (
+    <div className="App">
+      <h1>Hello world!!</h1>
+      <Potato />
+    </div>
+  );
 }
 
 export default App;
